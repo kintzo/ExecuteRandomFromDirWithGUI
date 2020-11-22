ef68d8b6d230365bebc9060644662981b1760c0d
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 13:47:41 +0200

    project creation

--- ExecuteRandomFromDirWithGUI/Form1.Designer.cs
+++ ExecuteRandomFromDirWithGUI/Form1.Designer.cs
@@ -28,20 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedLabel = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "add to list";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "select random executable";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "add to blacklist";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(302, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(215, 342);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected Executable:";
+            // 
+            // selectedLabel
+            // 
+            this.selectedLabel.AutoSize = true;
+            this.selectedLabel.Location = new System.Drawing.Point(12, 208);
+            this.selectedLabel.Name = "selectedLabel";
+            this.selectedLabel.Size = new System.Drawing.Size(0, 13);
+            this.selectedLabel.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(15, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Run";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(15, 263);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(145, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Delete";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 349);
+            this.ClientSize = new System.Drawing.Size(520, 349);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.selectedLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label selectedLabel;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- ExecuteRandomFromDirWithGUI/Form1.cs
+++ ExecuteRandomFromDirWithGUI/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace ExecuteRandomFromDirWithGUI
 {
@@ -15,6 +19,221 @@ namespace ExecuteRandomFromDirWithGUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void createList(Boolean add)
+        {
+            var foldersList = File.Exists("folders.txt") ? File.ReadAllLines("folders.txt").ToList() : new List<string>();
+
+            using (FolderBrowserDialog mainFolder = new FolderBrowserDialog())
+            {
+                DialogResult result = mainFolder.ShowDialog();
+
+                if (add && foldersList.IndexOf(mainFolder.SelectedPath) > -1)
+                {
+                    //Console.Clear();
+                    //Console.WriteLine();
+                    //Console.ReadLine();
+                    MessageBox.Show($"path \"{mainFolder.SelectedPath}\" already exists");
+                    return;
+                }
+
+                if (result == DialogResult.OK)
+                {
+                    if (add)
+                    {
+                        foldersList.Add(mainFolder.SelectedPath);
+                        File.WriteAllLines("folders.txt", foldersList);
+                    }
+                    else File.WriteAllText("folders.txt", mainFolder.SelectedPath);
+
+                    string[] exefiles = GetAllSafeFiles(mainFolder.SelectedPath, "*.exe");
+
+                    if (add)
+                    {
+                        List<string> exeList = File.ReadAllLines("output.txt").ToList();
+
+                        exeList.AddRange(exefiles);
+                        File.WriteAllLines("output.txt", exeList);
+                        listBox1.DataSource = exeList;
+                    }
+                    else
+                    {
+                        File.WriteAllLines("output.txt", exefiles);
+                        listBox1.DataSource = exefiles;
+                    }
+
+                }
+            }
+        }
+        public static string[] GetAllSafeFiles(string path, string searchPattern = "*.*")
+        {
+            List<string> allFiles = new List<string>();
+            string[] root = Directory.GetFiles(path, searchPattern);
+            allFiles.AddRange(root);
+
+            string[] folders = Directory.GetDirectories(path);
+            foreach (string folder in folders)
+            {
+                try
+                {
+                    if (!IsIgnorable(folder))
+                    {
+                        allFiles.AddRange(Directory.GetFiles(folder, searchPattern, SearchOption.AllDirectories));
+                    }
+                }
+                catch { } // Don't know what the problem is, don't care...
+            }
+
+            var blacklist = File.Exists("BlackList.txt") ? File.ReadAllLines("BlackList.txt").ToList() : new List<string>();
+            allFiles = allFiles.Where((x) =>
+            {
+                for (int i = 0; i < blacklist.Count; i++)
+                {
+                    if (x.ToUpper().Contains(blacklist[i].ToUpper())) return false;
+                }
+                return true;
+            }).ToList();
+
+            return allFiles.ToArray();
+        }
+
+        private static bool IsIgnorable(string dir)
+        {
+            if (dir.EndsWith("System Volume Information")) return true;
+            if (dir.Contains("$RECYCLE.BIN")) return true;
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createList(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            createList(true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("output.txt"))
+            {
+                var exeList = File.ReadAllLines("output.txt").ToList();
+                int index = new Random().Next(exeList.Count);
+
+                setSelectedItem(exeList[index]);
+                //manageExe(exeList, index);
+            }
+            else
+            {
+                //Console.Clear();
+                //Console.WriteLine("File list not found");
+                //Console.ReadLine();
+                MessageBox.Show("File list not found");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("output.txt")) {
+                List<string> exeList = File.ReadAllLines("output.txt").ToList();
+                listBox1.DataSource = exeList;
+            }        
+        }
+
+        private void setSelectedItem(string item) {
+            selectedLabel.Text = item;
+
+            button5.Enabled = item == "" ? false : true;
+            button6.Enabled = item == "" ? false : true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try {
+                StartProcess();
+            }
+            catch (Exception ex) {
+                var b = MessageBox.Show("Run application", ex.Message + " Delete?", MessageBoxButtons.YesNo);
+
+                if (b == DialogResult.Yes) {
+                    DeleteExeFromList();
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DeleteExeFromList();
+        }
+
+        void StartProcess()
+        {
+            var item = selectedLabel.Text;
+
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = item;
+            processInfo.WorkingDirectory = Path.GetDirectoryName(item);
+            processInfo.ErrorDialog = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardOutput = false;
+            processInfo.RedirectStandardError = false;
+            System.Diagnostics.Process.Start(processInfo);
+        }
+
+        void DeleteExeFromList()
+        {
+            var item = selectedLabel.Text;
+            setSelectedItem("");
+            var exeList = File.ReadAllLines("output.txt").ToList();
+            var list = exeList.Where(x => x != item).ToList();
+            listBox1.DataSource = list;
+
+            File.WriteAllLines("output.txt", list);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var BlackList = new List<string>();
+            if (File.Exists("BlackList.txt")) BlackList = File.ReadAllLines("BlackList.txt").ToList();
+
+            var input = "";
+            TextInputForm textDialog = new TextInputForm();
+            textDialog.label1.Text = "Enter blacklist word";
+            if (textDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                input = textDialog.textBox1.Text;
+            }
+            textDialog.Dispose();
+
+            if (input == "") {
+                MessageBox.Show("invalid word");
+                return;
+            }
+
+            if (BlackList.IndexOf(input) > -1)
+            {
+                MessageBox.Show($"\"{input}\" already exists in blacklist");
+            }
+            else
+            {
+                BlackList.Add(input);
+                var exeFiles = File.ReadAllLines("output.txt").ToList();
+
+                exeFiles = exeFiles.Where((x) =>
+                {
+                    for (int i = 0; i < BlackList.Count; i++)
+                    {
+                        if (x.ToUpper().Contains(BlackList[i].ToUpper())) return false;
+                    }
+                    return true;
+                }).ToList();
+
+                File.WriteAllLines("BlackList.txt", BlackList);
+                File.WriteAllLines("output.txt", exeFiles);
+                listBox1.DataSource = exeFiles;
+            }
         }
     }
 }
