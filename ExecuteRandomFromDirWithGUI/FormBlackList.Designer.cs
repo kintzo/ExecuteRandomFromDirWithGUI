4ed7552d24628aa6e64b61dbd3b2224922451f1f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 11:06:08 +0200

    τεστ

--- ExecuteRandomFromDirWithGUI/FormBlackList.Designer.cs
+++ ExecuteRandomFromDirWithGUI/FormBlackList.Designer.cs
@@ -40,16 +40,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(14, 14);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(212, 264);
+            this.listBox1.Size = new System.Drawing.Size(247, 304);
             this.listBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(230, 12);
+            this.button1.Location = new System.Drawing.Point(268, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(117, 27);
             this.button1.TabIndex = 1;
             this.button1.Text = "Add Word";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,9 +60,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(230, 41);
+            this.button2.Location = new System.Drawing.Point(270, 63);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.Size = new System.Drawing.Size(117, 27);
             this.button2.TabIndex = 2;
             this.button2.Text = "Remove Selected";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,9 +71,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(231, 252);
+            this.button3.Location = new System.Drawing.Point(270, 291);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.Size = new System.Drawing.Size(115, 27);
             this.button3.TabIndex = 3;
             this.button3.Text = "Close and Filter";
             this.button3.UseVisualStyleBackColor = true;
@@ -77,14 +82,15 @@
             // 
             // FormBlackList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 283);
+            this.ClientSize = new System.Drawing.Size(399, 327);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FormBlackList";
             this.Text = "BlackList";
             this.Load += new System.EventHandler(this.FormBlackList_Load);
